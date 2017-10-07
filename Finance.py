831d18ef8a516eadefd3574a4c4667e1885c5595
Kurt <kurtlprice@gmail.com>
2017-10-06 20:37:17 -0700

    Installed and implement flask_bootstrap for templates

--- Finance.py
+++ Finance.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, flash, url_for, render_template
+from flask_bootstrap import Bootstrap
 from blueprints import login_blueprint
 
 app = Flask(__name__)
@@ -6,12 +7,12 @@ site_name = "Finance Site"
 
 
 def index():
-    print("WTF")
     return render_template('index.html',
                            name=site_name)
 
 
 if __name__ == '__main__':
+    Bootstrap(app)
     app.session_cookie_name = site_name
     app.register_blueprint(login_blueprint.login_bp)
     app.run()
