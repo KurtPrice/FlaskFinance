6d90f80215722691d8e82e9abaf6671bfa22318b
Kurt <kurtlprice@gmail.com>
2017-11-14 00:14:16 -0800

    made cards_list populate from database rather than by a hardcoded set of cards.

--- Finance.py
+++ Finance.py
@@ -2,11 +2,16 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from blueprints import login_blueprint
+from database import create_database
 
 app = Flask(__name__)
 site_name = "Finance Site"
-cards_dict = {'CSR': 'Chase Sapphire Reserve', 'CF': 'Chase Freedom', 'CFU': 'Chase Freedom Unlimited',
-              'BN': 'Barnes & Noble', 'BKL': 'Buckle', 'AZN': 'Amazon', 'CSP': 'Chase Sapphire Preferred'}
+card_col = create_database("finance")
+card_col = card_col["cards"]
+card_list = card_col.find()
+cards_dict = {post["card"]: post["full_name"] for post in card_list}
+# cards_dict = {'CSR': 'Chase Sapphire Reserve', 'CF': 'Chase Freedom', 'CFU': 'Chase Freedom Unlimited',
+#              'BN': 'Barnes & Noble', 'BKL': 'Buckle', 'AZN': 'Amazon', 'CSP': 'Chase Sapphire Preferred'}
 
 
 if __name__ == '__main__':
